008972dc65c6133ef850c849af4873c2ed816cd1
jean-renoul <jean.renoul@laplateforme.io>
2024-05-06 16:03:50 +0200

    first sketch oh the menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <raylib.h>
-#include "ball.h"
+#include <iostream>
+#include "menu.hpp"
 
 int main()
 {
     Color darkGreen = Color{20, 160, 133, 255};
+    Color white = Color{255, 255, 255, 255};
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+    const int screenWidth = 1600;
+    const int screenHeight = 1200;
 
-    Ball ball = Ball();
-
+    Font GetFontDefault(void);
+    
     InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
     SetTargetFPS(60);
 
@@ -17,8 +19,19 @@ int main()
     {
         BeginDrawing();
         ClearBackground(darkGreen);
-        ball.Update();
-        ball.Draw();
+        DrawRectangleLines(buttonX, 75, buttonWidth, buttonHeight, white);
+        DrawRectangleLines(buttonX, 200, buttonWidth, buttonHeight, white);
+        DrawRectangleLines(buttonX, 325, buttonWidth, buttonHeight, white);
+        DrawRectangleLines(buttonX, 450, buttonWidth, buttonHeight, white);
+        DrawText("Lancer le jeu",200, 100, 50, white);
+        DrawText("Générer population aléatoire", 200, 225, 50, white);
+        DrawText("Lancement population par défaut", 200, 350, 50, white);
+        DrawText("Sauvegarder Population courante", 200, 475, 50, white);
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)&& GetMousePosition().x > 100 && GetMousePosition().x < 700 && GetMousePosition().y > 75 && GetMousePosition().y < 175)
+        {
+            DrawRectangle(100, 200, 600, 100, white);
+        }
         EndDrawing();
     }
 
---
+++ src/menu.hpp
@@ -0,0 +1,8 @@
+#ifndef MENU_HPP
+#define MENU_HPP
+
+const int buttonX = 100;
+const int buttonWidth = 1400;
+const int buttonHeight = 100;
+const int fontSize = 50;
+#endif
