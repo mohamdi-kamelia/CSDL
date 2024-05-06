08f2f524ac81e96fd15d5928a454f9c0ff0b76a4
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2024-05-06 16:13:16 +0200

    création de la grille

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,37 @@
-#include <raylib.h>
-#include "ball.h"
+#include "raylib.h"
 
-int main()
-{
-    Color darkGreen = Color{20, 160, 133, 255};
-
+int main() {
     const int screenWidth = 800;
     const int screenHeight = 600;
+    const int rows = 30; // Nombre de lignes
+    const int cols = 40; // Nombre de colonnes
+    const int cellSize = 20; // Taille de chaque cellule
 
-    Ball ball = Ball();
+    // Initialisation de la fenêtre raylib
+    InitWindow(screenWidth, screenHeight, "Grille de cellules");
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
+    SetTargetFPS(60); // Définit le framerate de la fenêtre
 
-    while (!WindowShouldClose())
-    {
+    // Boucle principale du jeu
+    while (!WindowShouldClose()) {
+        // Début du dessin
         BeginDrawing();
-        ClearBackground(darkGreen);
-        ball.Update();
-        ball.Draw();
+
+        ClearBackground(RAYWHITE);
+
+        // Dessin de la grille de cellules
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                DrawRectangle(j * cellSize, i * cellSize, cellSize, cellSize, LIGHTGRAY);
+                DrawRectangleLines(j * cellSize, i * cellSize, cellSize, cellSize, GRAY);
+            }
+        }
+
         EndDrawing();
     }
 
+    // Fermeture de la fenêtre raylib
     CloseWindow();
+
     return 0;
 }
